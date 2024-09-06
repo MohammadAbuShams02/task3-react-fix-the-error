f57ff2946a933c373635feab47827f9c2169cc3c
MohammadAbuShams02 <shamsmohammad966@gmail.com>
2024-09-06 18:19:14 +0300

    commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+export default function Contact() {
+  const [formInputs, setFormInput] = useState({
+    name: "",
+    email: "",
+    age: "",
+  });
+
+  const changeName = (e) => {
+    setFormInput({ ...formInputs, name: e.target.value });//
+  };
+  const changeEmail = (e) => {
+    setFormInput({ ...formInputs, email: e.target.value });//
+  };
+  const changeAge = (e) => {
+    setFormInput({ ...formInputs, age: e.target.value });//
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formInputs); //
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>User Name </label>
+      <input type="text" value={formInputs.name} onChange={changeName} />
+
+      <label>Email </label>
+      <input type="text" value={formInputs.email} onChange={changeEmail} />
+      <label>Age </label>
+      <input type="text" value={formInputs.age} onChange={changeAge} />
+
+      <hr />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
